0abe1d5dea514144e2cbf7d9a48db1ee59ac1757
hamid <hamire3a@gmail.com>
2021-07-27 19:51:18 +0430

    fix messages

--- User/Http/Requests/Auth/RegisterUserRequest.php
+++ User/Http/Requests/Auth/RegisterUserRequest.php
@@ -25,7 +25,7 @@ class RegisterUserRequest extends FormRequest
             /** username can contain alphabet, underline and digits */
             'sponsor_username' => ['required', 'exists:users,username', 'regex:/^[a-z][a-z0-9_]{2,}$/'],
             'username' => ['required', 'unique:users', 'regex:/^[a-z][a-z0-9_]{2,}$/'],
-            'password' => ['required', 'regex:/'.getSetting('USER_REGISTRATION_PASSWORD_CRITERIA').'/'],
+            'password' => ['required', 'regex:/' . getSetting('USER_REGISTRATION_PASSWORD_CRITERIA') . '/'],
             'password_confirmation' => 'required|string|same:password',
         ];
     }
@@ -33,18 +33,19 @@ class RegisterUserRequest extends FormRequest
     public function messages()
     {
         return [
-          'password_confirmation.same' => trans('user.validation.password-same'),
-          'first_name.required' => trans('user.validation.first-name-required'),
-          'last_name.required' => trans('user.validation.last-name-required'),
-          'email.required' => trans('user.validation.email-required'),
-          'email.unique:users' => trans('user.validation.email-unique'),
-          'sponsor_username.regex' => trans('user.validation.username-unique'),
-          'sponsor_username.required' => trans('user.validation.username-required'),
-          'username.unique:users' => trans('user.validation.username-unique'),
-          'username.required' => trans('user.validation.username-required'),
-          'username.regex' => trans('user.validation.username-regex'),
-          'password.required' => trans('user.validation.first-name-required'),
-          'email.email' => trans('user.validation.email-is-incorrect'),
+            'password_confirmation.same' => trans('user.validation.password-same'),
+            'first_name.required' => trans('user.validation.first-name-required'),
+            'last_name.required' => trans('user.validation.last-name-required'),
+            'email.required' => trans('user.validation.email-required'),
+            'email.unique:users' => trans('user.validation.email-unique'),
+            'sponsor_username.regex' => trans('user.validation.sponsor-username-unique'),
+            'sponsor_username.required' => trans('user.validation.sponsor-username-required'),
+            'sponsor_username.exists:users' => trans('user.validation.sponsor-username-not-exists'),
+            'username.unique:users' => trans('user.validation.username-unique'),
+            'username.required' => trans('user.validation.username-required'),
+            'username.regex' => trans('user.validation.username-regex'),
+            'password.required' => trans('user.validation.first-name-required'),
+            'email.email' => trans('user.validation.email-is-incorrect'),
         ];
     }
 }
