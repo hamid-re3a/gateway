f593ff257399b084fc45c5bddc66a1479a4ee412
Dariush Molaie <dariush.m1992@gmail.com>
2021-08-16 18:43:22 +0430

    change observer  ad produce changes users

--- User/Observers/UserObserver.php
+++ User/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace User\Observers;
 
+use App\Jobs\User\UserDataJob;
 use User\Mail\User\UserAccountActivatedEmail;
 use User\Jobs\TrivialEmailJob;
 use User\Models\User;
@@ -17,6 +18,19 @@ class UserObserver
                 'actor_id' => request()->user()->id
             ]);
             $history = $user->userHistories()->create($attributes);
+
+            if(!empty($user->isDirty())){
+                $userObject = new \User\Services\User();
+                $userObject->setId($user->id);
+                $userObject->setEmail($user->email);
+                $userObject->setFirstName($user->first_name);
+                $role_name = implode(",",$user->getRoleNames()->toArray());
+                $userObject->setRole($role_name);
+                $serializeUser = serialize($userObject);
+                UserDataJob::dispatch($serializeUser)->onQueue('subscriptions');
+                UserDataJob::dispatch($serializeUser)->onQueue('kyc');
+                UserDataJob::dispatch($serializeUser)->onQueue('mlm');
+            }
 
             if($user->isDirty('block_type')){
 
