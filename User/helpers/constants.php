bbacda7d381dbf343a7b4784dba3dfa5a70b2055
NimaN2D <nima.nouri.d@gmail.com>
2021-08-20 18:03:14 +0430

    Test transaction password

--- User/helpers/constants.php
+++ User/helpers/constants.php
@@ -122,7 +122,7 @@ const EMAIL_CONTENT_SETTINGS = [
         'variables_description'=>'full_name user full name, otp otp token',
         'type'=>'email',
     ],
-    'CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP'=>[
+    'TRANSACTION_PASSWORD_OTP'=>[
 
         'is_active' => true,
         'subject'=>'Transaction Password OTP',
