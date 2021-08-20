87b99f865baccea2e627568826e7ecb0daa675ab
NimaN2D <nima.nouri.d@gmail.com>
2021-08-20 17:40:06 +0430

    EmailChangeTransactionPasswordOTP log handler

--- User/Mail/User/ProfileManagement/EmailChangeTransactionPasswordOTP.php
+++ User/Mail/User/ProfileManagement/EmailChangeTransactionPasswordOTP.php
@@ -2,6 +2,7 @@
 
 namespace User\Mail\User\ProfileManagement;
 
+use Illuminate\Support\Facades\Log;
 use User\Mail\SettingableMail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
@@ -36,13 +37,18 @@ class EmailChangeTransactionPasswordOTP extends Mailable implements SettingableM
      */
     public function build()
     {
-//        $setting = $this->getSetting();
-        $setting = EmailContentSetting::query()->whereKey('CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP')->first();
-        $setting = $setting->toArray();
+        $setting = $this->getSetting();
+        $otpDuration = 60 ;
+        try{
+            if((is_null(secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION"))) || empty(secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION")))))
+                $otpDuration = secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION"));
+        } catch (\Throwable $exception) {
+            Log::error('TransactionPasswordOTP email error for getSetting USER_EMAIL_VERIFICATION_OTP_DURATION');
+        }
 
         $setting['body'] = str_replace('{{full_name}}',(is_null($this->user->full_name) || empty($this->user->full_name)) ? 'Unknown': $this->user->full_name,$setting['body']);
         $setting['body'] = str_replace('{{otp}}',(is_null(hyphenate($this->token)) || empty(hyphenate($this->token))) ? 'Unknown': hyphenate($this->token),$setting['body']);
-        $setting['body'] = str_replace('{{otp_expire_duration}}',(is_null(secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION"))) || empty(secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION")))) ? 'Unknown': secondsToHumanReadable(getSetting("USER_EMAIL_VERIFICATION_OTP_DURATION")),$setting['body']);
+        $setting['body'] = str_replace('{{otp_expire_duration}}',$otpDuration,$setting['body']);
 
         return $this
             ->from($setting['from'], $setting['from_name'])
@@ -52,6 +58,12 @@ class EmailChangeTransactionPasswordOTP extends Mailable implements SettingableM
 
     public function getSetting() : array
     {
-        return getEmailAndTextSetting('CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP');
+        try {
+            return getEmailAndTextSetting('CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP');
+        } catch (\Throwable $exception) {
+            Log::error('TransactionPasswordOTP email error for getSetting CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP');
+            $setting = EmailContentSetting::query()->whereKey('CHANGE_TRANSACTION_PASSWORD_EMAIL_OTP')->first();
+            return  $setting->toArray();
+        }
     }
 }
