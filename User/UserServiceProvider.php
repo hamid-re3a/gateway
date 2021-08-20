e36dd688159a1c71cb06c1f7e87a5ab707a7de3b
NimaN2D <nima.nouri.d@gmail.com>
2021-08-20 16:16:20 +0430

    cache clear test

--- User/UserServiceProvider.php
+++ User/UserServiceProvider.php
@@ -66,7 +66,7 @@ class UserServiceProvider extends ServiceProvider
      */
     protected function registerHelpers()
     {
-        if (file_exists($helperFile = __DIR__ . '/helpers/constants.php')) {
+        if (file_exists($helperFile = __DIR__ . './helpers/constants.php')) {
             require_once $helperFile;
         }
 
