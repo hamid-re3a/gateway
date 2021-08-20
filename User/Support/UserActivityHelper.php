21f5753107bce78785873a8b37a9306aee344dc8
NimaN2D <nima.nouri.d@gmail.com>
2021-08-20 18:13:22 +0430

    Test transaction password

--- User/Support/UserActivityHelper.php
+++ User/Support/UserActivityHelper.php
@@ -12,7 +12,7 @@ use User\Jobs\UrgentEmailJob;
 use User\Mail\User\ProfileManagement\EmailChangeTransactionPasswordOTP;
 use User\Mail\User\EmailVerifyOtp;
 use User\Mail\User\ForgetPasswordOtpEmail;
-use User\Mail\User\ProfileManagement\TransactionPasswordOtp;
+use User\Mail\User\ProfileManagement\TransactionPasswordOtpEmail;
 use User\Mail\User\WelcomeEmail;
 use User\Models\Agent as AgentModel;
 use User\Models\Ip;
@@ -192,7 +192,7 @@ class UserActivityHelper
                 "type" => OTP_TYPE_CHANGE_TRANSACTION_PASSWORD
             ]);
 
-            UrgentEmailJob::dispatch(new TransactionPasswordOtp($user, $token), $user->email);
+            UrgentEmailJob::dispatch(new TransactionPasswordOtpEmail($user, $token), $user->email);
             $data['token'] = $token;
             return [$data, $error];
 
