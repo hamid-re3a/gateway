25e4ad15b8c72f578910bb14beba241b592b4f05
Dariush Molaie <dariush.m1992@gmail.com>
2021-08-26 19:28:16 +0430

    additional user data migration

--- User/database/migrations/2015_06_19_172634_users_additional_field.php
+++ User/database/migrations/2015_06_19_172634_users_additional_field.php
@@ -15,6 +15,7 @@ class UsersAdditionalField extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('sponsor_id')->after('email')->unsigned()->nullable();
+            $table->unsignedBigInteger('member_id')->after('sponsor_id')->unsigned()->nullable();
         });
     }
 
