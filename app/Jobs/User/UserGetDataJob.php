5f14b8ee026ad0bbd77d5799398bb425af8ed2e8
Dariush Molaie <dariush.m1992@gmail.com>
2021-09-05 13:45:04 +0430

    change

--- app/Jobs/User/UserGetDataJob.php
+++ app/Jobs/User/UserGetDataJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use User\Services\UserAdminService;
 use User\Services\UserService;
 
@@ -31,6 +32,7 @@ class UserGetDataJob implements ShouldQueue
         $user_get_data_serialize = unserialize($this->data);
         $user_data = app(UserAdminService::class)->getUserData($user_get_data_serialize);
         $user_data_serialize = serialize($user_data);
+        Log::info("get user info and produce for service request sender",[$user_data]);
         UserDataJob::dispatch($user_data_serialize)->onConnection('rabbit')->onQueue($user_get_data_serialize->getQueueName());
     }
 }
