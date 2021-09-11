1487d5e7f70dfa82078e76afe934e834539a41c1
hamid <hamire3a@gmail.com>
2021-09-11 16:54:14 +0430

    update apth to vendor

--- clientGrpc.php
+++ clientGrpc.php
@@ -6,11 +6,11 @@ $client = new \User\Services\UserServiceClient('127.0.0.1:9595', [
     'credentials' => \Grpc\ChannelCredentials::createInsecure()
 ]);
 $request = new \User\Services\Id();
-$request->setId((int)3);
+$request->setId((int)1);
 
 
 list($reply, $status) = $client->getUserById($request)->wait();
 
-print_r($status);
-print_r($reply->getFirstName());
-$getdata = $reply->getGetdataarr();
+print_r($status->code);
+//print_r($reply);
+//$getdata = $reply->getGetdataarr();
