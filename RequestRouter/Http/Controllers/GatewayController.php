32fe9ef1d7c14ea9224a2e4e1c07fa407718feea
hamid <hamire3a@gmail.com>
2021-10-19 19:06:51 +0330

    fix array header

--- RequestRouter/Http/Controllers/GatewayController.php
+++ RequestRouter/Http/Controllers/GatewayController.php
@@ -275,8 +275,12 @@ class GatewayController extends Controller
         $res = $req->$method($final_route);
         if (in_array($res->header('Content-Type'), ALL_MIME_TYPES)) {
             if (!$multi)
-                foreach ($res->headers() as $key => $value)
-                    header("$key: $value");
+                foreach ($res->headers() as $key => $value) {
+                    if (is_array($value))
+                        header("$key: ".implode(';',$value));
+                    else
+                        header("$key: $value");
+                }
             echo $res->body();
             return null;
         }
