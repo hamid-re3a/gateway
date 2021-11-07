eaf478dffea5eb00deb2cf25dc847776d2758939
hamid <hamire3a@gmail.com>
2021-11-07 14:39:23 +0330

    add seeders and update user gender

---
+++ User/Convert/Models/IndividualDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace User\Convert\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Individual extends Model
+{
+    protected $guarded = [];
+
+    protected $table = '2297_ft_individual';
+    protected $connection = 'conversion_mysql';
+}
