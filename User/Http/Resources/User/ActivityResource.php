c12922350ff15431eddeaef158fbcdd9e88a6b56
NimaN2D <nima.nouri.d@gmail.com>
2021-11-22 12:42:26 +0330

    Translates improvements

--- User/Http/Resources/User/ActivityResource.php
+++ User/Http/Resources/User/ActivityResource.php
@@ -23,7 +23,7 @@ class ActivityResource extends JsonResource
             'device' => !empty($this->agent_id) ? $this->agent->device_type : 'Unknown',
             'platform' => !empty($this->agent_id) ? $this->agent->platform . '(' . $this->agent->platform_version .')' : 'Unknown',
             'browser'=> !empty($this->agent_id) ? $this->agent->browser . ' ' . $this->agent->browser_version : 'Unknown',
-            'action' => getDbTranslate($this->route, $this->route)
+            'action' => getDbTranslate($this->route)
         ];
     }
 }
